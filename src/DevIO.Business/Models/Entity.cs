074f67cba60df6876510f2aec37921175595136d
BernardoModerno <bernardo.moderno@gmail.com>
2020-07-03 22:45:34 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIO.Business/Models/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevIO.Business.Models
+{
+    
+    //abstract pq a classe não pode ser instanciada, apenas herdada
+    public abstract class Entity 
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DevIO.Business.Models
+{
+    public class Produto : Entity
+    {
+        /* Informação dizendo que o produto pertence a um fornecedor */
+        public Guid FornecedorId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Imagem { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        /*EF Relation relacao com fornecedor 1 fornecedor tem varios produtos*/
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
